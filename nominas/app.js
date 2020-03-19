79ab109ca40cda6deb2f2a3efc353cf7061702e7
Ivan Mercedes <ivanmercede@gmail.com>
2020-03-19 09:29:39 -0400

    Initial commit

--- nominas/app.js
+++ nominas/app.js
@@ -4,12 +4,27 @@ const app = document.getElementById('app');
 
 const opciones = {
     "language": {
-        "lengthMenu": "Mostrar _MENU_ por pagina",
-        "zeroRecords": "Nothing found - sorry",
+        "lengthMenu": "Mostrar _MENU_ registros",
+        "zeroRecords": "No se encontraron resultados",
         "info": "Pagina _PAGE_ de _PAGES_",
-        "infoEmpty": "No ha registro displonible",
-        "infoFiltered": "( filtrado de _MAX_ )",   
-    }
+        "sLengthMenu":     "Mostrar _MENU_ registros",
+        "sZeroRecords":    "No se encontraron resultados",
+        "sEmptyTable":     "Ningún dato disponible en esta tabla =(",
+        "sInfo":           "Mostrando registros del _START_ al _END_ de un total de _TOTAL_ registros",
+        "sInfoEmpty":      "Mostrando registros del 0 al 0 de un total de 0 registros",
+        "sInfoFiltered":   "(filtrado de un total de _MAX_ registros)",
+        "sInfoPostFix":    "",
+        "sSearch":         "Buscar:",
+        "sUrl":            "",
+        "sInfoThousands":  ",",
+        "sLoadingRecords": "Cargando...",
+        "oPaginate": {
+            "sFirst":    "Primero",
+            "sLast":     "Último",
+            "sNext":     "Siguiente",
+            "sPrevious": "Anterior"   
+        }
+    },          
 }
 
 
